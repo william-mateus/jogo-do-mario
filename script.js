3e339bbe379bd70a1f5024d8c1e03f4dcaab0c67
william-mateus <william.mtt003@gmail.com>
2023-07-24 18:48:52 -0300

    mudancas no css e scrpt

--- script.js
+++ script.js
@@ -1,159 +1,192 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const clouds = document.querySelector('.clouds');
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
 const elementoMensagem = document.getElementById("mensagem");
+const gameBoard = document.querySelector(".game-board");
+
 let score = 0;
 let marioMorreu = false;
 let sonic = false;
-var audioSonic = new Audio('2-GreenHillZone.mp3');
-var audioMario = new Audio('SuperMarioBros.mp3');
-var audioPokemon = new Audio('audioPokemon.mp3');
-var audioDonkey = new Audio('audioDonkey.mp3');
-var audioStreetFigther = new Audio('audioStreetFighter.mp3');
-var audioNaruto = new Audio('audioNaruto.mp3');
-var audioMorreu = new Audio('BossTheme.mp3');
+var audioMorreu = new Audio("BossTheme.mp3");
+
+const personagens = [
+	{
+		nome: "mario",
+		src: "mario.gif",
+		background: "fundoMario.webp",
+		largura: "150px",
+		marginLeft: "0px",
+        alturaChao: "65px",
+		audio: new Audio("SuperMarioBros.mp3"),
+	},
+	{
+		nome: "sonic",
+		src: "sonic.gif",
+		background: "fundoSonic.png",
+		largura: "80px",
+		marginLeft: "50px",
+        alturaChao: "90px",
+		audio: new Audio("2-GreenHillZone.mp3"),
+	},
+	{
+		nome: "pokemon",
+		src: "pikachu.gif",
+		background: "fundoPokemon.png",
+		largura: "120px",
+		marginLeft: "50px",
+        alturaChao: "65px",
+		audio: new Audio("audioPokemon.mp3"),
+	},
+	{
+		nome: "street fighter",
+		src: "ken.gif",
+		background: "fundoKen.gif",
+		largura: "200px",
+		marginLeft: "50px",
+        alturaChao: "0px",
+		audio: new Audio("audioStreetFighter.mp3"),
+	},
+	{
+		nome: "naruto",
+		src: "naruto.gif",
+		background:"fundoNaruto.jpg",
+		largura:"200px",
+		marginLeft:"50px",
+		alturaChao:"0px",
+		audio: new Audio("audioNaruto.mp3"),
+	}
+];
+let personagemAtual = personagens[0];
 
 function mostrarMensagem(mensagem) {
-    elementoMensagem.innerText = mensagem;
-    elementoMensagem.classList.remove("escondido"); 
-        
-    elementoMensagem.classList.add("paraAnimacao");
-        
-    setTimeout(() => {
-        elementoMensagem.classList.remove("paraAnimacao");
-    }
-    , 1000);
+	elementoMensagem.innerText = `SCORE: ${mensagem}`;
+    // TODO: mostrar mensagem de game over
+    // TODO: Adicionar botão de reiniciar
+    // TODO: Adicionar botão de iniciar
+    // TODO: Centralizar game-board
+    // TODO: Mudar pipe de acordo com o personagem
+    // TODO: Adicionar image de morte de acordo com o personagem
+    // TODO: Parar animação do background quando o personagem morre
+    // TODO: Organizar arquivos por pasta
 }
 
 function jump() {
-    mario.classList.add('jump');
+	mario.classList.add("jump");
 
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 500);
+	setTimeout(() => {
+		mario.classList.remove("jump");
+	}, 500);
 }
 
 function jumpMobile() {
-    mario.classList.add('jumpMobile');
+	mario.classList.add("jumpMobile");
 
-    setTimeout(() => {
-        mario.classList.remove('jumpMobile');
-    }, 500);
+	setTimeout(() => {
+		mario.classList.remove("jumpMobile");
+	}, 500);
 }
-
-
 
 function loop() {
-    if (!marioMorreu) {
-        
-        
-       
-        const pipePosition = pipe.offsetLeft;
-        const cloudsPosition = clouds.offsetLeft;
-        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+	if (!marioMorreu) {
+		const pipePosition = pipe.offsetLeft;
+		const cloudsPosition = clouds.offsetLeft;
+		const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
-        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-            marioMorreu = true;
-            pipe.style.animation = 'none';
+        console.log(pipePosition, cloudsPosition, marioPosition);
 
-            pipe.style.left = `${pipePosition}px`;
+		/*if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+			marioMorreu = true;
+			pipe.style.animation = "none";
+			pipe.style.left = `${pipePosition}px`;
 
-            mario.style.animation = 'none';
+            mario.src = "morreu.png";
+            mario.style.animation = "none";
+			mario.style.bottom = `${marioPosition}px`;
+			mario.style.width = "75px";
+			mario.style.marginLeft = "50px";
 
-            mario.style.bottom = `${marioPosition}px`;
-
-            clouds.style.animation = 'none';
-
-            clouds.style.left = `${cloudsPosition}px`;
-
-            mario.src = 'morreu.png'
-            mario.style.width = '75px'; 
-            mario.style.marginLeft = '50px';
-            
+			clouds.style.animation = "none";
+			clouds.style.left = `${cloudsPosition}px`;
         }
-            //fazer foto de quando sonic morre
-        if (score > 5 && pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ){
-            marioMorreu = true;
-            pipe.style.animation = 'none';
 
-            pipe.style.left = `${pipePosition}px`;
+		//fazer foto de quando sonic morre
+		/* if (score > 5 && pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+			marioMorreu = true;
+			pipe.style.animation = "none";
+			pipe.style.left = `${pipePosition}px`;
 
-            mario.style.animation = 'none';
-
-            mario.style.bottom = `${marioPosition}px`;
-
-            mario.src = 'sonicMorreu.png'
-            mario.style.width = '75px'; 
-            mario.style.marginLeft = '50px';
-        }
-        //parar audio se morreu
-    }
+            mario.src = "sonicMorreu.png";
+			mario.style.animation = "none";
+			mario.style.bottom = `${marioPosition}px`;
+			mario.style.width = "75px";
+			mario.style.marginLeft = "50px";
+		} */
+	}
 }
 
-function audioStop(audio){
-    audio.pause();
-audio.currentTime = 0;
+function audioStop(audio) {
+	audio.pause();
+	audio.currentTime = 0;
+}
+
+function mudarPersonagem(personagem) {
+    personagemAtual.audio.pause();
+
+    document.startViewTransition(() => {
+        mario.src = personagem.src;
+        mario.style.width = personagem.largura;
+        mario.style.marginLeft = personagem.marginLeft;
+        mario.style.bottom = personagem.alturaChao;
+
+        pipe.style.bottom = personagem.alturaChao;
+
+        gameBoard.setAttribute("data-bg", personagem.background);
+        gameBoard.style.backgroundImage = `url(${personagem.background})`;
+    });
+
+    personagem.audio.play();
+    personagemAtual = personagem;
 }
 
 function pontuacao() {
-    if (!marioMorreu) {
-        score++;
-        console.log("Pontuacao!");
-        
+	if (!marioMorreu) {
+		score++;
+		console.log("Pontuacao!");
+        elementoMensagem.classList.add("animado");
+        setTimeout(() => {
+            elementoMensagem.classList.remove("animado");
+        }, 250);
+	}
+
+    const indicePersonagem = Math.min(Math.floor(score / 20), personagens.length - 1);
+    console.log(indicePersonagem);
+    const personagem = personagens[indicePersonagem];
+
+    if (personagemAtual != personagem) {
+        mudarPersonagem(personagem);
     }
-    if(score < 6){
-        audioMario.play();
-    }
-    //sonic
-    if ( score == 9){
-        audioMario.pause()
-        mario.src = 'sonic.gif'
-        mario.style.width = '80px'; 
-        mario.style.marginLeft = '50px'; 
-        
 
+	//som quando morre!
+	if (marioMorreu) {
+        for (const personagem of personagens) {
+            personagem.audio.pause();
+        }
 
-        audioSonic.play();
-     }
-     if ( score == 19){
-        audioSonic.pause()
-        mario.src = 'pikachu.gif'
-        mario.style.width = '120px'; 
-        mario.style.marginLeft = '50px';  
-        audioPokemon.play();
-     }
-     if ( score == 29){
-        audioPokemon.pause()
-        mario.src = 'ken.gif'
-        mario.style.width = '200px'; 
-        mario.style.marginLeft = '50px';  
-        audioStreetFigther.play();
-     }
-     if ( score == 39){
-        audioStreetFigther.pause()
-        mario.src = 'naruto.gif'
-        mario.style.width = '200px'; 
-        mario.style.marginLeft = '50px';  
-        audioNaruto.play();
-     }
-        // donkey consertar!!
-     
-     //som quando morre!
-     if(marioMorreu){
-        audioMario.pause();
-        audioSonic.pause();
-        audioPokemon.pause();
-        audioStreetFigther.pause();
-        audioNaruto.pause();
-        audioMorreu.play();
-     }
-   
-     mostrarMensagem(score);
+		audioMorreu.play();
+	}
 
+	mostrarMensagem(score);
 }
 
-
 setInterval(loop, 10);
-setInterval(pontuacao, 2000);
-document.addEventListener('keydown', jump);
-document.addEventListener('click',jump);
+
+// Check if the user is using a mobile device
+if (window.matchMedia("(max-width: 768px)").matches) {
+    setInterval(pontuacao, 900);
+} else {
+    setInterval(pontuacao, 2000);
+}
+
+mudarPersonagem(personagemAtual);
+document.addEventListener("keydown", jump);
+document.addEventListener("click", jump);
